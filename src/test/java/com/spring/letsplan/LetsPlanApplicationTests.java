e3b5a03850d2b48c73a5bc8ce14e86d3d8fa6795
chaewon <jcw3241@gmail.com>
2025-02-19 14:50:57 +0900

    0219 개인 사이드 프로젝트 : 계획을세우새우 최초커밋

---
+++ src/test/java/com/spring/letsplan/LetsPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.letsplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
