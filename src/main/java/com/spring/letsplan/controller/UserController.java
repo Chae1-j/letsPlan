fba97506d690df4cfecbe55f1c4aba2096aac11a
chaewon <jcw3241@gmail.com>
2025-04-15 17:57:16 +0900

    0415 로그인 페이지 수정 및 추가 중

--- src/main/java/com/spring/letsplan/controller/UserController.java
+++ src/main/java/com/spring/letsplan/controller/UserController.java
@@ -2,6 +2,7 @@ package com.spring.letsplan.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -10,13 +11,29 @@ public class UserController {
         return "/user/mypage-main";
     }
 
+    // 로그인 페이지
     @GetMapping("/login")
     public String login() {
         return "/user/login";
     }
 
-    @GetMapping("/register")
+    // 회원가입 페이지
+    @PostMapping("/register")
     public String register() {
         return "/user/register";
     }
+
+    // 아이디 찾기
+    @PostMapping("/findId")
+    public String findId() {
+        return null;
+    }
+
+    // 비밀번호 찾기
+    @PostMapping("/chngPwd")
+    public String chngPwd() {
+        return null;
+    }
+
+
 }
